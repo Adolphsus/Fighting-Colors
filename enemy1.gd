beed00922eb5fc2b562f2c0533e90b596e016164
Janoequide <alejandro.mori@ug.uchile.cl>
2023-04-24 21:54:55 -0400

    Cambios a enemy1
    Variables de vida agregadas, barra de vida agregada, muerte agregada

--- enemy1.gd
+++ enemy1.gd
@@ -4,7 +4,35 @@ class_name Enemy
 @onready var animation_tree = $AnimationTree
 @onready var animation_player = $AnimationPlayer
 @onready var playback = animation_tree.get("parameters/playback")
+@onready var progress_bar = $MarginContainer/ProgressBar
 
+const Max_health = 100
+var health = 100:
+	set(value):
+		health = value
+		progress_bar.value = value
+	get:
+		return health
+		
+enum { SEEK,
+ATTACK,
+DEATH
+}
+
+var state = SEEK
+
+func _ready():
+	progress_bar.value = Max_health
+
+		
 func take_damage():
-	print('o')
+	health = max(health - 25, 0)
 	playback.travel("hurt")
+	if health == 0:
+		playback.travel("knockdown")
+		state = DEATH
+		
+func _physics_process(delta):
+	if state == DEATH:
+		await get_tree().create_timer(1.3).timeout
+		queue_free()
