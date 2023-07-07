93243b6f788b43c74621ceff69f31ec44079328d
Adolphsus <129559220+Adolphsus@users.noreply.github.com>
2023-07-07 16:48:34 -0400

    cámara condición de muerte

--- Scenes/main_camera.gd
+++ Scenes/main_camera.gd
@@ -12,15 +12,14 @@ func limits(x, y):
 	var tween = create_tween().set_parallel().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "limit_left", x, 2.5)
 	tween.tween_property(self, "limit_right", y, 2.5)
-	pass
 	
 func _ready():
 	Game.camera = self
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var A = max(player.global_position.x, player_2.global_position.x) * 3/4 + min(player.global_position.x, player_2.global_position.x) * 1/4
-	position.x = A
-	$"physic limits".global_position = get_screen_center_position()
+	if player.alive and player_2.alive:
+		var A = max(player.global_position.x, player_2.global_position.x) * 3/4 + min(player.global_position.x, player_2.global_position.x) * 1/4
+		position.x = A
+		$"physic limits".global_position = get_screen_center_position()
